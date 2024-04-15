3766135b4dac7037051b12c823792899d6b2a777
yuri <arc.bunny.121@gmail.com>
2024-04-15 13:41:22 +0900

    login機能作成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,3 +4,12 @@
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
+@section('content')
+<div class="login__content">
+  <div class="login-form__heading">
+    <h2>店舗情報</h2>
+  </div>
+
+ 
+</div>
+@endsection
