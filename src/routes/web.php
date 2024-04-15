0b79e34bf3d2d5ed8ebd3c1bf95db7434f036ccd
yuri <arc.bunny.121@gmail.com>
2024-04-15 14:08:03 +0900

    店舗一覧用コントローラー作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', [AuthController::class, 'index']);});
+Route::get('/', [AuthController::class, 'index']);
