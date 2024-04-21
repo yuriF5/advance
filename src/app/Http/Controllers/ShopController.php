feaa03e98eb6ab58b653dd1ec6dc1a92ae64be62
yuri <arc.bunny.121@gmail.com>
2024-04-21 18:12:22 +0900

    インポート

--- src/app/Http/Controllers/ShopController.php
+++ src/app/Http/Controllers/ShopController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Shop;
+use App\Models\Area;
+use App\Models\Favorite;
+use App\Models\Genre;
+use Illuminate\Support\Facades\Auth;
 
 class ShopController extends Controller
 {
