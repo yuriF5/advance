73dd75553b4f94065316afecf20551e2c3eb9258
yuri <arc.bunny.121@gmail.com>
2024-04-21 17:59:41 +0900

    作成

--- src/database/migrations/2024_04_21_173951_create_favorites_table.php
+++ src/database/migrations/2024_04_21_173951_create_favorites_table.php
@@ -15,9 +15,8 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrainde()->cascadeOnDelete();
-            $table->foreignId('shop_id')->constrainde()->cascadeOnDelete();
-            $table->string('status');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('shop_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
