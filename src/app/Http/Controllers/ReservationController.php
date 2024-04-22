5fd2a12e3a83ba0a740e746cc277d4e5a0b3b649
yuri <arc.bunny.121@gmail.com>
2024-04-22 21:58:37 +0900

    修正

--- src/app/Http/Controllers/ReservationController.php
+++ src/app/Http/Controllers/ReservationController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Favorite;
+use App\Models\Reservation;
+use App\Models\Shop;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $reservation = new Reservation();
+        $reservation->shop_id = $shop->id;
+        $reservation->user_id = Auth::user()->id;
+        $reservation->date = $request->input('date');
+        $reservation->time = $request->input('time');
+        $reservation->number_of_people = $request->input('number_of_people');
+        $reservation->status = "予約";
+        $reservation->save();
+
+        return redirect('/done');
+    }
 }
--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -4,3 +4,17 @@
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/detail.css') }}">
 @endsection
+
+@section('content')
+    <div class="detail__wrap">
+        <div class="detail__header">
+            <div class="header__title">
+                <a href="{{ $backRoute }}" class="header__back"><</a>
+                <span class="header__shop-name">{{ $shop->name }}</span>
+            </div>
+        </div>
+        <div class="detail__image">
+            <img src="{{ $shop->image_url }}" alt="イメージ画像" class="detail__image-img">
+        </div>
+    </div>
+@endsection
