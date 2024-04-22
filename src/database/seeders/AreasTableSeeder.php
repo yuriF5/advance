f8c9a2b22623e3cb4fc77f1ab36a0607bac16ec2
yuri <arc.bunny.121@gmail.com>
2024-04-22 19:40:16 +0900

    作成

---
+++ src/database/seeders/AreasTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AreasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
