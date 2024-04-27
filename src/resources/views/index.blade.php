049bd04963b9b1a666ad5e70f3527703e6b45d15
yuri <arc.bunny.121@gmail.com>
2024-04-27 13:05:42 +0900

    飲食店一覧css変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,69 +4,98 @@
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
+
 @section('header')
-    <form class="header__right" action="/" method="get">
 
-        <div class="header__search">
-            <label class="select-box__label">
-                <select name="area" class="select-box__item">
-                    <option value="">All area</option>
-                    @foreach ($areas as $area)
-                        <option class="select-box__option" value="{{ $area->id }}" {{ request('area') == $area->id ? 'selected' : '' }}>{{ $area->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </label>
-
-            <label class="select-box__label">
-                <select name="genre" class="select-box__item">
-                    <option value="">All genre</option>
-                    @foreach ($genres as $genre)
-                        <option value="{{ $genre->id }}" {{ request('genre') == $genre->id ? 'selected' : '' }}>
-                            {{ $genre->name }}</option>
-                    @endforeach
-                </select>
-            </label>
-
-            <div class="search__item">
-                <div class="search__item-button"></div>
-                <label class="search__item-label">
-                    <input type="text" name="word" class="search__item-input" placeholder="Search ..." value="{{ request('word') }}">
-                </label>
-            </div>
-        </div>
-    </form>
 @endsection
 
+
+
 @section('content')
-<div class="flex justify-between flex-wrap">
+    <div class="header__right">
+        <div class="search__icon">
+
+            <div class="overlay"></div>
+            <form class="header__search-form" action="/" method="get">
+                @csrf
+                <div class="header__search">
+                    <label class="select-box__label">
+                        <select name="area" class="select-box__item">
+                            <option value="">All area</option>
+                            @foreach ($areas as $area)
+                                <option class="select-box__option" value="{{ $area->id }}" {{ request('area') == $area->id ? 'selected' : '' }}>{{ $area->name }}</option>
+                            @endforeach
+                        </select>
+                    </label>
+
+                    <label class="select-box__label">
+                        <select name="genre" class="select-box__item">
+                            <option value="">All genre</option>
+                            @foreach ($genres as $genre)
+                                <option value="{{ $genre->id }}" {{ request('genre') == $genre->id ? 'selected' : '' }}>{{ $genre->name }}</option>
+                            @endforeach
+                        </select>
+                    </label>
+
+                    <div class="search__item">
+                        <div class="search__item-button"></div>
+                        <label class="search__item-label">
+                            <input type="text" name="word" class="search__item-input" placeholder="Search ..." value="{{ request('word') }}">
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="shop__wrap">
+
     @if (!empty($message))
     <p>{{ $message }}</p>
     @endif
+
     @foreach ($shops as $shop)
-    <div class="w-56 bg-white rounded-md shadow-md mb-4">
-        <div>
-            <img class="w-full h-28 object-cover rounded-t-md" src="{{ $shop->image_url }}" alt="{{ $shop->name }}">
+        <div class="shop__content">
+            <div>
+                <img class="shop__image" src="{{ $shop->image_url }}" alt="{{ $shop->name }}">
+            </div>
+            <div class="shop__item">
+                <h2 class="shop__title">{{ $shop->name }}</h2>
+                <div class="shop__tag">
+                    <span class="shop__tag-info">#{{ $areaNames[$shop->area_id] }}</span>
+                    <span class="shop__tag-info">#{{ $genreNames[$shop->genre_id] }}</span>
+                </div>
+                <div class="shop__button">
+                    <a class="shop__button-detail" href="{{ url('/detail/'.$shop->id) }}">詳しくみる</a>
+                    @if (Auth::check())
+                        @if (in_array($shop->id, $favorites))
+                            <form method="POST" action="{{ url('/favorite') }}" class="shop__button-favorite form">
+                                @csrf
+                                @method('delete')
+                                <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+                                <input type="hidden" name="shop_id" value="{{ $shop->id }}">
+                                <button type="submit" class="shop__button-favorite-btn" title="お気に入り削除">
+                                    <img class="favorite__btn-image" src="{{ asset('images/heart_color.svg') }}">
+                                </button>
+                            </form>
+                        @else
+                            <form method="POST" action="{{ url('/favorite') }}" class="shop__button-favorite form">
+                                @csrf
+                                <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+                                <input type="hidden" name="shop_id" value="{{ $shop->id }}">
+                                <button type="submit" class="shop__button-favorite-btn" title="お気に入り追加">
+                                    <img class="favorite__btn-image" src="{{ asset('images/heart.svg') }}">
+                                </button>
+                            </form>
+                        @endif
+                    @else
+                        <button type="button" onclick="location.href='/login'" class="shop__button-favorite-btn">
+                            <img class="favorite__btn-image" src="{{ asset('images/heart.svg') }}">
+                        </button>
+                    @endif
+                </div>
+            </div>            
         </div>
-        <div class="p-3">
-            <h2 class="font-bold">{{ $shop->name }}</h2>
-            <div class="text-xs">
-                <span>#{{ $areaNames[$shop->area_id] }}</span>
-                <span>#{{ $genreNames[$shop->genre_id] }}</span>
-            </div>
-            <div class="flex justify-between items-center mt-2">
-                <a class="text-xs h-6 rounded-md bg-blue-600 text-white px-3 pt-1" href="{{ url('/detail/'.$shop->id) }}">詳しくみる</a>
-                @if( Auth::check() )
-                    <form method="POST" action="{{ url('/favorite') }}">
-                        @csrf
-                        <input type="hidden" name="user_id" value="{{ Auth::id() }}">
-                        <input type="hidden" name="shop_id" value="{{ $shop->id }}">
-                        <button class="text-2xl {{ $shop->favorites->contains('user_id', Auth::id()) ? 'text-red-500' : 'text-gray-100' }}" type="submit">&#9829;</button>
-                    </form>
-                @endif
-            </div>
-        </div>            
-    </div>
     @endforeach
-</div>
+</main>
 @endsection
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,45 +1,46 @@
 <!DOCTYPE html>
 <html lang="ja">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Rese</title>
-        <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        @yield('css')
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('css')
+</head>
 
-    <body>
-        <header class="header__l">
-            <div class="header__icon">
-                <input id="header__input" class="header__hidden" type="checkbox">
-                <label for="header__input" class="header__open" ><span></span></label>
-                    <nav class="header-nav__content">
-                        <ul class="header-nav">
-                            <li class="header-nav__item"><a class="header-nav__link" href="/">Home</a></li>
-                        @if (Auth::check())
-                            <li class="header-nav__item"><a class="header-nav__link" href="/mypage">Mypage</a>
-
-                            <li class="header-nav__item"><form class="form" action="{{ route('logout') }}" method="POST" style="display: inline;">
+<body>
+    <header class="header__l">
+        <div class="header__icon">
+            <input id="header__input" class="header__hidden" type="checkbox">
+            <label for="header__input" class="header__open"><span></span></label>
+            <nav class="header-nav__content">
+                <ul class="header-nav">
+                    <li class="header-nav__item"><a class="header-nav__link" href="/">Home</a></li>
+                    @if (Auth::check())
+                    <li class="header-nav__item"><a class="header-nav__link" href="/mypage">Mypage</a></li>
+                    <li class="header-nav__item">
+                        <form class="form" action="{{ route('logout') }}" method="POST" style="display: inline;">
                             @csrf
                             <button type="submit" class="header-nav__link" style="background:none; border:none; padding:0; color:blue; cursor:pointer;">Logout</button>
-                            </form>
-                        @else
-                            <li class="header-nav__item"><a class="header-nav__link" href="/register">Registration</a></li>
-                            <li class="header-nav__item"><a class="header-nav__link" href="/login">Login</a></li>
-                        @endif
-                        </ul>
-                    </nav>
-                    <div class="header__logo">Rase</div>
-                </div>
-            </div>
-            @yield('header')
-        </header>
+                        </form>
+                    </li>
+                    @else
+                    <li class="header-nav__item"><a class="header-nav__link" href="/register">Registration</a></li>
+                    <li class="header-nav__item"><a class="header-nav__link" href="/login">Login</a></li>
+                    @endif
+                </ul>
+            </nav>
+            <div class="header__logo">Rase</div>
+        </div>
+    </header>
 
-        <main>
-            @yield('content')
-        </main>
-    </body>
+    <main>
+        @yield('content')
+    </main>
+
+    @yield('header')
+</body>
 
 </html>
