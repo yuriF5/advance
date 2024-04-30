f9d5d7aef5e03dc080ea6d7c80922ff4e2f9e3fd
yuri <arc.bunny.121@gmail.com>
2024-04-30 17:51:03 +0900

    予約変更機能作成

--- src/app/Http/Controllers/ReservationController.php
+++ src/app/Http/Controllers/ReservationController.php
@@ -19,17 +19,33 @@ class ReservationController extends Controller
             'number' => 'required|numeric|min:1|max:5',
         ]);
 
-        // 予約データの作成
+        // 既存の予約があるかどうかを確認
+    $reservation = Reservation::where('shop_id', $shop->id)
+                                ->where('user_id', Auth::id())
+                                ->first();
+
+    // 予約データの作成または更新
+    if ($reservation) {
+        // 既存の予約がある場合は更新
+        $reservation->update([
+            'date' => $request->date,
+            'time' => $request->time,
+            'number_of_people' => $request->number,
+        ]);
+    } else {
+        // 既存の予約がない場合は新規作成
         $reservation = new Reservation();
         $reservation->shop_id = $shop->id;
-        $reservation->user_id = Auth::id(); // 認証されたユーザーのIDを使用する
+        $reservation->user_id = Auth::id(); 
         $reservation->date = $request->date;
         $reservation->time = $request->time;
         $reservation->number_of_people = $request->number;
         $reservation->save();
+    }
 
-        // 完了ページにリダイレクト
-        return redirect()->route('done');
+    // 完了ページにリダイレクト
+    return redirect()->route('done');
+
     }
 
     public function done()
