844231b01ca458552f8ed1db113d64ca4d940afa
yuri <arc.bunny.121@gmail.com>
2024-05-02 20:13:15 +0900

    ダミー作成

--- src/database/factories/ReviewFactory.php
+++ src/database/factories/ReviewFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Shop;
 class ReviewFactory extends Factory
 {
     /**
@@ -13,8 +13,16 @@ class ReviewFactory extends Factory
      */
     public function definition()
     {
+        $stars = [1, 2, 3, 4, 5];
         return [
-            //
+            'user_id' => 2,
+            'shop_id' => function () {
+                return Shop::inRandomOrder()->first()->id;
+            },
+            'star' => $this->faker->randomElement($stars),
+            'comment' => $this->faker->sentence(), 
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Review;
 use App\Models\Favorite;
 use App\Models\Reservation;
 use App\Models\User;
+use App\Models\Shop;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,16 +20,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        User::factory(10)->create();
+        // User::factory(10)->create();
         
-    $this->call([
-            AreasTableSeeder::class,
-            GenresTableSeeder::class,
-            ShopsTableSeeder::class, 
-        ]);
+    // $this->call([
+    //         AreasTableSeeder::class,
+    //         GenresTableSeeder::class,
+    //         ShopsTableSeeder::class, 
+    //     ]);
 
-        Favorite::factory(10)->create();
-        Reservation::factory(10)->create();
+        // Favorite::factory(10)->create();
+        // Reservation::factory(10)->create();
+        Review::factory(10)->create();
 
 }
 }
