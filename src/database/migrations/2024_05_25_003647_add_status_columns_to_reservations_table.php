76593e445f9dadd5a185c2076c1420fa962e3b84
yuri <arc.bunny.121@gmail.com>
2024-05-25 01:10:32 +0900

    statusカラムを追加

--- src/database/migrations/2024_05_25_003647_add_status_columns_to_reservations_table.php
+++ src/database/migrations/2024_05_25_003647_add_status_columns_to_reservations_table.php
@@ -7,26 +7,23 @@ use Illuminate\Support\Facades\Schema;
 class AddStatusColumnsToReservationsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * visit_statusは来店と未来店で強制キャンセルの管理、payment_statusは決済完了or未完了の管理をするため追加作成
      *
      * @return void
      */
     public function up()
     {
         Schema::table('reservations', function (Blueprint $table) {
-            //
+            $table->enum('visit_status', ['予約中', '来店済み', '強制キャンセル'])->default('予約中')->after('number_of_people');
+            $table->enum('payment_status', ['支払い済み', '未払い'])->default('未払い')->after('visit_status');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('reservations', function (Blueprint $table) {
-            //
+            $table->dropColumn('visit_status');
+            $table->dropColumn('payment_status');
         });
-    }
+}
 }
